43d2ccf843cbd9feee67fb84b9e34ee1bd9d2449
hyeon <vikim1210@naver.com>
2023-04-13 16:05:22 +0900

    테스트 생성할 때도 일급 컬렉션을 전달 받는 생성자 사용

--- MVC_Framework_Basic/GPA_Example/src/main/java/GradeCalculator.java
+++ MVC_Framework_Basic/GPA_Example/src/main/java/GradeCalculator.java
@@ -3,9 +3,14 @@ import java.util.List;
 public class GradeCalculator {
     //1급 컬렉션 변수 생성
     private final Courses courses;
-//매개변수는 테스트 하려는 클래스
+    //매개변수는 테스트 하려는 클래스
     public GradeCalculator(List<Course> courses) {
         this.courses = new Courses(courses);
+    }
+
+    // 1급 컬렉션으로 처음부터 받기
+    public GradeCalculator(Courses courses){
+        this.courses = courses;
     }
 
     public double calculateGrade() {
--- MVC_Framework_Basic/GPA_Example/src/test/java/GradeCalculatorTest.java
+++ MVC_Framework_Basic/GPA_Example/src/test/java/GradeCalculatorTest.java
@@ -12,8 +12,8 @@ import org.junit.jupiter.api.Test;
 public class GradeCalculatorTest {
     //학점 계산기 도메인: 이수 과목, 학점 계산기
     //객체 관계 & 도메인 모델링: 이수 과목(프로그래밍,자료구조,중국어 회화) -> 과목(코스) 클래스
-    //협력(요청)과 적절한 책임: 이수한 과목을 전달하여 평균 학점 계산 요청 -> 학점 계산기 -> (학점수+교과목 평점)의 합계 -> 과목(코스)
-    //                                                                      -> 수강신청 총 학점 수        -> 과목(코스
+    //협력(요청)과 적절한 책임: 이수한 과목을 전달하여 평균 학점 계산 요청 -> 학점 계산기 -> (학점수+교과목 평점)의 합계 -> 과목(코스) 일급 컬렉션
+    //                                                                      -> 수강신청 총 학점 수        -> 과목(코스) 일급 컬렉션
 
    @DisplayName("평균 학점을 계산한다")
     @Test
@@ -21,7 +21,7 @@ public class GradeCalculatorTest {
         List<Course> courses = List.of(new Course("OOP", 3, "A"),
             new Course("자료구조",3,"A+"));
 
-        GradeCalculator gradeCalculator = new GradeCalculator(courses);
+        GradeCalculator gradeCalculator = new GradeCalculator(new Courses(courses));
         double gradeResult = gradeCalculator.calculateGrade();
 
         assertThat(gradeResult).isEqualTo(4.25);
