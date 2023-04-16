bc5039e1d4271ff7fd0577f3d3ff6d9d7ff5e36f
hyeon <vikim1210@naver.com>
2023-04-17 01:05:00 +0900

    메뉴와 요리 연결 확인하는 테스트 코드 완성, 메소드 구현은 미완성

--- MVC_Framework_Basic/Serving_Example/src/main/java/MenuItem.java
+++ MVC_Framework_Basic/Serving_Example/src/main/java/MenuItem.java
@@ -1,9 +1,9 @@
-public class Cook {
+public class MenuItem {
 
     private final String name;
     private final int price;
 
-    public Cook(String name, int price) {
+    public MenuItem(String name, int price) {
         this.name = name;
         this.price = price;
     }
