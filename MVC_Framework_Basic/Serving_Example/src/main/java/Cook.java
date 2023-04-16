0bb04d180cdfe2c5decaeb3e2d0daee6ef9c1df3
hyeon <vikim1210@naver.com>
2023-04-17 01:11:00 +0900

    객체를 비교할 때는 isEqualTo 사용하되, 오버라이딩 필수_테스트 코드 통과 하도록 수정 완

--- MVC_Framework_Basic/Serving_Example/src/main/java/Cook.java
+++ MVC_Framework_Basic/Serving_Example/src/main/java/Cook.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Cook {
 
     private final String name;
@@ -6,5 +8,22 @@ public class Cook {
     public Cook(String name, int price) {
         this.name = name;
         this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Cook cook = (Cook) o;
+        return price == cook.price && Objects.equals(name, cook.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
     }
 }
