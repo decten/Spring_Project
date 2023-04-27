d5095eef02f848740751e16d8aee0475ae3dda8b
hyeon <vikim1210@naver.com>
2023-04-27 21:26:59 +0900

    Logger 환경 설정 오류 해결

---
+++ MVC_Framework_Basic/Web_Calculator/src/main/java/CustomerWebApplicationServer.java
@@ -0,0 +1,15 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CustomerWebApplicationServer {
+    private final int port;
+    private static final Logger logger = LoggerFactory
+
+    public CustomerWebApplicationServer(int port) {
+        this.port = port;
+    }
+
+    public void start(){
+
+    }
+}
