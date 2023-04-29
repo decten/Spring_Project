22bf6dbf8c5bd8dd13bb753f6a544170a05610f0
hyeon <vikim1210@naver.com>
2023-04-29 22:06:24 +0900

    쿼리 스트링 추출

--- MVC_Framework_Basic/Web_Calculator/src/main/java/org/example/ReqeustLine.java
+++ MVC_Framework_Basic/Web_Calculator/src/main/java/org/example/ReqeustLine.java
@@ -1,8 +1,21 @@
 package org.example;
 
+//    GET /calculate?operand1=11&operator=*&operand2=55 HTTP/1.1
 public class ReqeustLine {
 
-    public ReqeustLine(String reqeustLine) {
+    private final String method; // GET
+    private final String urlPath; // /calculate?operand1=11&operator=*&operand2=55
+    private String queryString;
 
+    public ReqeustLine(String reqeustLine) {
+        String[] tokens = reqeustLine.split(" ");
+        this.method = tokens[0];
+
+        String[] urlTokens = tokens[1].split("\\?");
+        this.urlPath = urlTokens[0];
+        //쿼리 스트링이 존재하는 경우
+        if(urlTokens.length == 2){
+            this.queryString = urlTokens[1];
+        }
     }
 }
