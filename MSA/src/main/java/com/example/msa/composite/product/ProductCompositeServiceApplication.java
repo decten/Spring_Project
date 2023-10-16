1671b1f9646adb6475a387ffcced2cf770956836
hyun <vikim1210@naver.com>
2023-10-17 00:36:58 +0900

    [Add] ProductCompositeServiceApplication: composite 패키지에 구현 코드 작성 & 의존성 추가

---
+++ MSA/src/main/java/com/example/msa/composite/product/ProductCompositeServiceApplication.java
@@ -0,0 +1,19 @@
+package com.example.msa.composite.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spring.web.plugins.Docket;
+import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
+import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
+
+@SpringBootApplication
+public class ProductCompositeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductCompositeServiceApplication.class, args);
+    }
+
+}
