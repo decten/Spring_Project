a4e6b2ce9420d9ce7ca1c5a6fa31c4663768155b
hyun <vikim1210@naver.com>
2023-10-17 00:37:33 +0900

    [Add] ProductCompositeServiceApplicationTest: composite 패키지 구현 코드의 테스트 코드 작성 & 의존성 추가

---
+++ MSA/src/test/java/com/example/msa/composite/product/ProductCompositeServiceApplicationTest.java
@@ -0,0 +1,16 @@
+package com.example.msa.composite.product;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import static reactor.core.publisher.Mono.just;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductCompositeServiceApplicationTest {
+    @Test
+    public void getProductById() {
+    }
+}
